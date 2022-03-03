98585bf1e48f28af110083f6acff48eb8ffe7d49
Onion Limited <onionltd@protonmail.com>
2022-03-03 19:51:04 +0000

    Add missing QR code in DepositCard
    Closes #4.

--- src/components/DepositCard.vue
+++ src/components/DepositCard.vue
@@ -6,19 +6,33 @@
 
         <el-row>
             <el-col class="text-center">
-                <h1>!!QR CODE!!</h1>
+                <canvas ref="canvas" width="145" height="145" :title="address"></canvas>
             </el-col>
         </el-row>
 
+        <br>
+
         <el-row>
-            <el-col class="text-center">
+            <el-col class="text-center break">
                 {{ address }}
             </el-col>
         </el-row>
     </el-card>
 </template>
 
+<style scoped>
+    .break {
+        word-wrap: break-word;
+    }
+
+    .text-center {
+        text-align:center;
+    }
+</style>
+
 <script>
+    import qrcode from "qrcode"
+
     export default {
         Name: "DepositCard",
 
@@ -28,6 +42,20 @@
 
         emits: [
             "back"
-        ]
+        ],
+
+        async mounted() {
+            const ctx = this.$refs.canvas.getContext("2d")
+
+            let qrImage = new Image()
+            qrImage.onload = () => {
+                ctx.drawImage(qrImage, 0, 0)
+            }
+            qrImage.src = await qrcode.toDataURL(this.address, {
+                errorCorrectionLevel: 'L',
+                width: 145,
+                margin: 0,
+            })
+        }
     }
 </script>
