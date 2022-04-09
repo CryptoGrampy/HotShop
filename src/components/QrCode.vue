b1e81fef06138829467012ab7220570cd64e429e
CryptoGrampy <cryptogrampy@protonmail.com>
2022-04-08 23:14:47 -0500

    Fix QR

--- src/components/QrCode.vue
+++ src/components/QrCode.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
-import VueQrious from 'vue-qrious'
 import { simplePay } from '../main';
+import VueQrcode from '@chenfengyuan/vue-qrcode';
 
 const props = defineProps<{
   moneroAddress: string
   xmrAmount: number
 }>()
 
+const qrValue = `monero:${props.moneroAddress}?tx_amount=${String(simplePay.convertAtomicUnitsToXmr(String(props.xmrAmount)))}`
 </script>
 <template>
-    <vue-qrious :size="200" :value="`monero:${props.moneroAddress}?tx_amount=${String(simplePay.convertAtomicUnitsToXmr(String(props.xmrAmount)))}`" />
+  <vue-qrcode :value="qrValue" :options="{ width: 200 }"></vue-qrcode>
 </template>
