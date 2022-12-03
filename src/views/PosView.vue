205f899504f40913a68ad2025311618ce102d726
CryptoGrampy <cryptogrampy@protonmail.com>
2022-12-02 21:59:51 -0600

    Add Monerod loading text.  Resolves #10

--- src/views/PosView.vue
+++ src/views/PosView.vue
@@ -1,16 +1,25 @@
 <script setup lang="ts">
 import ReceiveComponent from "../components/ReceiveComponent.vue";
-import { simplePayReady } from "../SimplePay";
+import {
+  monerodConnectionStatusText,
+  simplePayReady,
+} from "../SimplePay";
 import HeaderComponent from "../components/HeaderComponent.vue";
+import { computed } from "@vue/reactivity";
+
+
+const statusText = computed(() => {
+  if (simplePayReady.value === true) {
+    return false;
+  } else {
+    return { text: monerodConnectionStatusText.value };
+  }
+});
 </script>
 
 <template>
   <HeaderComponent />
-  <el-row
-    v-loading="!simplePayReady"
-    justify="center"
-    element-loading-text="Setting up HotShop..."
-  >
+  <el-row v-loading="statusText" justify="center">
     <el-col :span="24">
       <ReceiveComponent />
     </el-col>
